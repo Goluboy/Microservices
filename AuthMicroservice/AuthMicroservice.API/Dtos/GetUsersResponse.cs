13748fe277b0e0d586574cc4ffbe0b0185fbd860
Goluboy <vaaalera12321@gmail.com>
2025-10-04 07:33:52 +0500

    README and .gitignore

--- AuthMicroservice/AuthMicroservice.API/Dtos/GetUsersResponse.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace AuthMicroservice.API.Dtos;
-
-
-public record GetUsersResponse(IList<UsersWithRoles> users);
-public record UsersWithRoles(Guid Id, string FirstName, string LastName, string Email, IList<string> Roles);
